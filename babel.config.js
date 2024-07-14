0dba83100dc73b99f3f316704eb22acda3ad5bc4
Chris Tokmakidis <anapdev@gmail.com>
2024-07-14 15:56:43 +0300

    packages

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+	presets: ['module:@react-native/babel-preset'],
+	plugins: [
+			[
+					'module-resolver',
+					{
+							root: ['./'],
+							alias: {
+									'@': './',
+							},
+					},
+			],
+			'module:react-native-dotenv',
+			'react-native-reanimated/plugin',
+	],
 };
