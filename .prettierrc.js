62a85ee7788ca0ade2c3900f255494f45ea472dc
Chris Tokmakidis <anapdev@gmail.com>
2024-07-14 15:56:14 +0300

    update prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+	printWidth: 90,
+	tabWidth: 4,
+	trailingComma: 'all',
+	bracketSpacing: true,
+	bracketSameLine: false,
+	singleQuote: true,
+	arrowParens: 'avoid',
+	endOfLine: 'auto',
 };
